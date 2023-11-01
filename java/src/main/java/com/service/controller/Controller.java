af2ff8280590cb5413b38e22eab1d74d8f1178d0
Bilgin Ibryam <bibryam@gmail.com>
2023-11-01 10:24:05 +0000

    Updated Java sample to use HTTP client for request/reply

--- java/src/main/java/com/service/controller/Controller.java
+++ java/src/main/java/com/service/controller/Controller.java
@@ -1,15 +1,20 @@
 package com.service.controller;
 
-import io.dapr.Topic;
 import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
 import io.dapr.client.domain.CloudEvent;
 
-import io.dapr.client.domain.HttpExtension;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 import io.dapr.client.domain.State;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,13 +28,20 @@ import javax.annotation.PostConstruct;
 public class Controller {
     private static final Logger logger = LoggerFactory.getLogger(Controller.class);
     private DaprClient client;
+    private HttpClient httpClient;
 
     private static final String PUBSUB_NAME = System.getenv().getOrDefault("PUBSUB_NAME", "pubsub");
     private static final String KVSTORE_NAME = System.getenv().getOrDefault("KVSTORE_NAME", "kvstore");
+    private static final String DAPR_HTTP_ENDPOINT = System.getenv().getOrDefault("DAPR_HTTP_ENDPOINT", "http://localhost");
+    private static final String DAPR_API_TOKEN = System.getenv().getOrDefault("DAPR_API_TOKEN", "");
 
     @PostConstruct
     public void init() {
         client = new DaprClientBuilder().build();
+        httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
     }
 
     // Publish messages 
@@ -68,8 +80,20 @@ public class Controller {
     public Mono<ResponseEntity> request(@RequestBody(required = true) Order order) {
         return Mono.fromSupplier(() -> {
             try {
-                Order response = client.invokeMethod("target", "invoke/neworders", order, HttpExtension.POST, Order.class).block();
-                logger.info("Invoke Successful. Reply received: " + response.getOrderId());
+                JSONObject obj = new JSONObject();
+                obj.put("orderId", order.getOrderId());
+
+                HttpRequest request = HttpRequest.newBuilder()
+                        .POST(HttpRequest.BodyPublishers.ofString(obj.toString()))
+                        .uri(URI.create(DAPR_HTTP_ENDPOINT + "/invoke/neworders"))
+                        .header("dapr-api-token", DAPR_API_TOKEN)
+                        .header("Content-Type", "application/json")
+                        .header("dapr-app-id", "target")
+                        .build();
+
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                JSONObject jsonObject = new JSONObject(response.body());
+                logger.info("Invoke Successful. Reply received: " + jsonObject.getInt("orderId"));
                 return ResponseEntity.ok("SUCCESS");
             } catch (Exception e) {
                 logger.error("Error occurred while invoking App ID. " + e);
