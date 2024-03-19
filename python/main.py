868c5730e0f6f765894e8e93aaf080e594274e07
Luis Rascao <luis.rascao@gmail.com>
2024-03-19 10:33:44 +0000

    python: add opentelemetry tracing support

--- python/main.py
+++ python/main.py
@@ -7,14 +7,22 @@ import grpc
 import requests
 import os
 
+from opentelemetry import trace
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.grpc import GrpcInstrumentorClient
+
 app = FastAPI()
+
+FastAPIInstrumentor.instrument_app(app)
 
 logging.basicConfig(level=logging.INFO)
 
-
 class Order(BaseModel):
-    orderId: int
-
+    orderId: str
+    productId: int
+    quantity: int
 
 class CloudEvent(BaseModel):
     datacontenttype: str
@@ -28,43 +36,62 @@ class CloudEvent(BaseModel):
     type: str
     traceid: str
 
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+# Get the tracer
+tracer = trace.get_tracer(__name__)
+
+grpc_client_instrumentor = GrpcInstrumentorClient()
+grpc_client_instrumentor.instrument()
 
 # Set up required inputs for http client to perform service invocation
-base_url = os.getenv('DAPR_HTTP_ENDPOINT', 'http://localhost')
+http_endpoint = os.getenv('DAPR_HTTP_ENDPOINT', 'http://localhost')
+grpc_endpoint = os.getenv('DAPR_GRPC_ENDPOINT', 'http://localhost')
 dapr_api_token = os.getenv('DAPR_API_TOKEN', '')
 pubsub_name = os.getenv('PUBSUB_NAME', 'pubsub')
 kvstore_name = os.getenv('KVSTORE_NAME', 'kvstore')
 invoke_target_appid = os.getenv('INVOKE_APPID', 'target')
 
-
 @app.get('/')
 async def helloworld():
-    return {"Hello World"}
-
+    return {"DAPR_HTTP_ENDPOINT": http_endpoint,
+            "DAPR_GRPC_ENDPOINT": grpc_endpoint,
+            "DAPR_API_TOKEN": dapr_api_token,
+            "PUBSUB_NAME": pubsub_name,
+            "KVSTORE_NAME": kvstore_name,
+            "INVOKE_APPID": invoke_target_appid}
 
 @app.post('/pubsub/orders')
 async def publish_orders(order: Order):
-    with DaprClient() as d:
-        try:
-            result = d.publish_event(
-                pubsub_name=pubsub_name,
-                topic_name='orders',
-                data=order.model_dump_json(),
-                data_content_type='application/json',
-            )
-            logging.info('Publish Successful. Order published: %s' %
-                         order.orderId)
-            return {'success': True}
-        except grpc.RpcError as err:
-            logging.error(
-                f"Error occurred while publishing order: {err.code()}")
-
+    with tracer.start_as_current_span(name='publish') as span:
+        with DaprClient() as d:
+            try:
+                result = d.publish_event(
+                    pubsub_name=pubsub_name,
+                    topic_name='orders',
+                    data=order.model_dump_json(),
+                    data_content_type='application/json',
+                )
+                logging.info('Publish Successful. Order published: %s' %
+                             order.orderId)
+                return {'success': True}
+            except grpc.RpcError as err:
+                logging.error(
+                    f"Error occurred while publishing order: {err.code()}")
 
 @app.post('/pubsub/neworders')
 def consume_orders(event: CloudEvent):
-    logging.info('Order received : %s' % event.data['orderId'])
-    return {'success': True}
-
+    logging.info('Order #%s received' % event.data['orderId'])
+    with tracer.start_as_current_span(name='neworder') as span:
+        with DaprClient() as d:
+            try:
+                d.save_state(store_name=kvstore_name,
+                             key=str(event.data['orderId']), value=str(event.data))
+                return {"success": True}
+            except grpc.RpcError as err:
+                print(f"Error={err.details()}")
+                raise HTTPException(status_code=500, detail=err.details())
+        return {'success': True}
 
 @app.post('/invoke/orders')
 async def send_order(order: Order):
@@ -72,7 +99,7 @@ async def send_order(order: Order):
                'content-type': 'application/json'}
     try:
         result = requests.post(
-            url='%s/invoke/neworders' % (base_url),
+            url='%s/invoke/neworders' % (http_endpoint),
             data=order.model_dump_json(),
             headers=headers
         )
@@ -107,7 +134,6 @@ def create_kv(order: Order):
         except grpc.RpcError as err:
             print(f"Error={err.details()}")
             raise HTTPException(status_code=500, detail=err.details())
-
 
 @app.get('/kv/orders/{orderId}')
 def get_kv(orderId: int):
