62b299920bb5840032eb8610e2c6d6578258f3e3
Alice Gibbons <alice@diagrid.io>
2024-05-16 15:06:07 +0100

    Workflow round 1

---
+++ csharp/Activities/CreateGreetingActivity.cs
@@ -0,0 +1,21 @@
+using Dapr.Workflow;
+
+namespace BasicWorkflowSamples
+{
+    public class CreateGreetingActivity : WorkflowActivity<string, string>
+    {
+        public override Task<string> RunAsync(WorkflowActivityContext context, string name)
+        {
+            Console.WriteLine($"{nameof(CreateGreetingActivity)} with input: {name}");
+
+            var greetings = new []{"Hello", "Hi", "Hey", "Hola", "Bonjour", "Ciao", "Guten Tag", "Konnichiwa"};
+            var selectedGreeting = greetings[new Random().Next(0, greetings.Length)];
+            var message = $"{selectedGreeting} {name}";
+
+            Thread.Sleep(5000); // Simulate a longer running workflow
+            Console.WriteLine($"{nameof(CreateGreetingActivity)} generated message is: {message}");
+
+            return Task.FromResult(message);
+        }
+    }
+}
